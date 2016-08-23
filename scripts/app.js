e2c67751908dce3112623f7dfc1356cdcaf702e7
Payam Yousefi <payousefi@users.noreply.github.com>
2016-08-23 14:12:49 -0700

    formatting + scope updates

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,8 @@
   * by Payam Yousefi | last updated August 21, 2016
   */
 
-'use strict'; // Keep global scope happy
-
 var MusicApp = function(user, key, wrap, content){
+  'use strict';
   // Initialize some variables
   var self = this;
   this.username = user;
@@ -15,26 +14,30 @@ var MusicApp = function(user, key, wrap, content){
 
   // Make the last.fm API request
   this.go = function() {
-    'use strict';
     var baseURL = "http://ws.audioscrobbler.com/2.0/";
     var method = "user.getTopArtists";
     var limit = 12;
     var period = "1month"; // Last 30 days
-    var queryURL = baseURL+"?method="+method+"&user=" + self.username + "&api_key="+self.apikey+"&limit="+limit+"&period="+period+"&format=json";
+    var queryURL = baseURL+"?method="+method+"&user=" + self.username +
+                    "&api_key=" + self.apikey+"&limit="+limit+
+                    "&period="+period+"&format=json";
     $.getJSON(queryURL, self.printData);
   };
 
   this.printData = function(data) {
-    'use strict';
     var html = '';
-    if (!data.error && data.topartists && data.topartists.artist.length != 0) {
+    if (!data.error && data.topartists && data.topartists.artist.length !== 0) {
       // Generate each artist tile
       $.each(data.topartists.artist, function(i, item) {
         var imgsrc = '';
-        if (item.image && item.image[4] && item.image[4]["#text"] != "") {
+        if (item.image && item.image[4] && item.image[4]["#text"] !== "") {
           imgsrc = 'style="background-image:url(' + item.image[4]["#text"] + ');" ';
         }
-        html += '<a href="' + item.url + '"><div class="artist" ' + imgsrc + 'title="' + item.name + '"><div class="title">' + item.name + '<span>' + item.playcount + ' plays</span></div><div class="dark"></div></div></a> ';
+        html += '<a href="' + item.url + '">'+
+                '<div class="artist" ' + imgsrc + 'title="' + item.name + '">'+
+                '<div class="title">' + item.name +
+                '<span>' + item.playcount + ' plays</span></div>'+
+                '<div class="dark"></div></div></a> ';
       });
 
       // Fade in new background color
@@ -48,13 +51,15 @@ var MusicApp = function(user, key, wrap, content){
 
     } else {
       // Load error message
-      html = '<p><em>User was not found on last.fm, or there was not enough data from the past month available.</em></p>';
+      html = '<p><em>User was not found on last.fm, or there was not'+
+             ' enough data from the past month available.</em></p>';
     }
 
     // Append and show data
     self.fill.append(html);
     self.fill.slideDown(2000);
   };
+
 };
 
 // JQuery loaded?
@@ -64,14 +69,19 @@ $(document).ready(function() {
 
   /* Detect URL routing */
   var path = window.location.pathname.split('/');
-  var username = (path[1] != null && path[1] != "") ? path[1] : "solitude12";
+  var username = (path[1] !== null && path[1] !== "") ? path[1] : "solitude12";
 
   // Initialize and run the app
   var app = new MusicApp(username, "***REMOVED***", "#wrap", ".content");
   app.go();
 
   // Share
-  $(".social").jsSocials({shareIn: "popup",showLabel: false,showCount: false,shares: ["facebook", "email", "twitter", "googleplus"]});
+  $(".social").jsSocials({
+    shareIn: "popup",
+    showLabel: false,
+    showCount: false,
+    shares: ["facebook", "email", "twitter", "googleplus"]
+  });
 
   /* Form functionality */
   $("#username").keypress(function(event) {
